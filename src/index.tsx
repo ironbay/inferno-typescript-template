b4a5a84c1debbb81b9a0f75d80ddfaa22ef69821
Dax Ravi <d@ironbay.digital>
2017-03-26 15:15:36 -0400

    Upgrade

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
-import * as createElement from 'inferno/dist/inferno-create-element'
-import * as Inferno from 'inferno'
+import Inferno from 'inferno'
 import { routes } from './routes'
 
 Inferno.render(routes, document.getElementById('inferno'))
